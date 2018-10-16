903e2326cadefdfc3e1c0ca1680c1293d554821f
luis <lfflorian4@gmail.com>
2018-10-15 23:04:42 -0600

    Finalizado primera version de mailapp

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 var app = angular.module('App', ['ngRoute']);
+var rutaApi = 'http://localhost:3030/api/';
+var ubicacion = null;
 
 //Rutas
 app.config(function($routeProvider) {
@@ -17,26 +19,105 @@ app.config(function($routeProvider) {
 })
 
 //Form
-app.controller('mailController', function($scope) {
-    $scope.var = 'Mail'
+app.controller('mailController', function($scope, $http) {
+    function obtenerDatos(){
+        $http({
+            method: 'GET',
+            url: rutaApi + 'receptores'
+        }).then(function sucess(response){
+            $scope.destinatarios = response.data
+        }, function error(response) {
+
+        });
+    }
+
+    $scope.EnviarCorreo = function() {
+        $scope.mostrarLoading = true;
+        var mensaje = new Object;
+        mensaje.correo = $scope.seleccion;
+        mensaje.mensaje = $scope.mensaje;
+        mensaje.asunto = $scope.asunto;
+
+        $http({
+            method: 'POST',
+            url: rutaApi + 'enviar',
+            data: mensaje,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST'
+        }).then(function sucess(response) {
+            limpiarCampos()
+            $scope.mostrarResultado = true;
+            $scope.mensajeResultado = 'Mensaje enviado satisfactoriamente';
+        }, function error(response) {
+            $scope.mostrarResultado = true;
+            $scope.mensajeResultado = 'No se pudo enviar el mensaje al destinatario';
+        })
+
+        $scope.mostrarLoading = false;
+    }
+
+    function limpiarCampos() {
+        $scope.seleccion = '';
+        $scope.mensaje = '';
+        $scope.asunto = '';
+    }
+
+    obtenerDatos()
 })
 
 //Destinatario
 app.controller('destController', function($scope, $http) {
     $scope.GuardarDestinatario = function() {
-        var datos = "{ 'nombre': '"+ $scope.Nombre +"'  , 'correo': '"+ $scope.Correo +"'  }";
-        console.log(JSON.stringify(datos))
+        $scope.mostrarLoading = true;
+        var destinatario = new Object;
+        destinatario.nombre = $scope.Nombre;
+        destinatario.correo = $scope.Correo;
 
         $http({
             method: 'POST',
-            url: 'http://localhost:300/api/receptor',
-            data: JSON.stringify(datos)
+            url: rutaApi + 'receptor',
+            data: destinatario,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST'
         }).then(function sucess(response) {
-            console.log("Correcto")
-            console.log(response)
+            limpiarCampos()
+            $scope.mostrarResultado = true;
+            $scope.mensajeResultado = 'Datos almacenados correctamente';
         }, function error(response) {
-            console.log("Error")
-            console.log(response)
+            $scope.mostrarResultado = true;
+            $scope.mensajeResultado = 'No se pudo almacenar los datos';
         })
+
+        $scope.mostrarLoading = false;
+    }
+
+    function limpiarCampos() {
+        $scope.Nombre = '';
+        $scope.Correo = '';
     }
 })
+
+//Directivas
+app.directive('loading', function () {
+    return {
+        restrict: 'AE',
+        template: '<div class="form-group text-center">'+
+                    '<div class="lds-ripple"><div></div><div></div></div>'+
+                  '</div>'
+    };
+});
+
+app.directive('resultado', function () {
+    return {
+        restrict: 'AE',
+        template: '<div class="form-group">' +
+                    '<div class="card bg-dark text-white">'+
+                        '<div class="card-body">{{ mensajeResultado }}</div>'+
+                    '</div>'+
+                  '</div>'
+    };
+});
